50d0b07e9f70dc92b38273d105bbbac755b7c2d8
kimtaeho <xxxxoghxxx@naver.com>
2019-10-08 15:11:40 +0900

    addPugAndSetViews

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import path from "path";
 const app = express();
 
 app.use(helmet());
+app.use("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser, json());
 app.use(morgan("tiny"));
